4bc269e35580a409c0fcb0c5604189b63bd89a38
bradh43 <brad.hodkinson2@gmail.com>
2020-03-21 19:13:10 -0500

    update

--- cube-solver/src/solver/Solver.java
+++ cube-solver/src/solver/Solver.java
@@ -7,16 +7,20 @@ package solver;
 import java.util.*;
 
 import cube.Algorithm;
+import cube.*;
+
 public class Solver {
 
 	public static void main(String[] args) {
 		
-//		Cube cube = new Cube(2);
-//		System.out.println(cube);
-//		
-//		Move move = new Move(3);
-//		System.out.println(move);
+		Cube cube = new Cube(2);
+		System.out.println(cube);
+		
+		Cube testCube = new Cube(3);
+		System.out.println(testCube);
+		
 		String moves = "U\' L D\' R2 D2 R B D\' U\' F2 R U2 B2 D2 F2 U F\' U\' F2 R\' F2 R L F\' U";
+		
 		Algorithm test =  new Algorithm();
 		test.generateScramble(2, 20);
 		System.out.println(test);
