5f376c0dab66317a32e3fa80bf4eb5d6b8249fcb
Admin <Admin@Admins-MacBook-Pro.local>
2020-04-05 21:12:13 -0500

    Completed Cube Explorer

--- cube-solver/src/viz/CubeExplorer.java
+++ cube-solver/src/viz/CubeExplorer.java
@@ -2,7 +2,9 @@ package viz;
 
 import java.util.*;
 
+import cube.Algorithm;
 import cube.Cube;
+import cube.Move;
 
 public class CubeExplorer {
 
@@ -14,13 +16,20 @@ public class CubeExplorer {
 		Cube cube = new Cube(cubeSize);
 		displayCubeUsageMessage();
 		System.out.println(cube);
-
+		startExplorer(cube);
 
 	}
 
+	
+	
 	private static void displayCubeUsageMessage() {
 		System.out.println("\nType \"scramble\" to mix the cube up");
-		System.out.println("Type \"solve\" to solve the cube\n");
+		System.out.println("Type \"solve\" to solve the cube");
+		System.out.println("Type \"new\" to generate a new cube");
+		System.out.println("Type \"size\" to change the size of the cube");
+		System.out.println("Type \"usage\" to view usage message");
+		System.out.println("Type \"quit\" to quit Cube Explorer\n");
+		
 		
 		System.out.println("Enter valid notation to twist the cube. Notation listed below:");
 		String[] faces = new String[]{"up", "right", "front", "down", "left", "back"};
@@ -30,6 +39,8 @@ public class CubeExplorer {
 		}
 		System.out.println();
 	}
+	
+	
 	
 	private static int getUsersCubeSize(Scanner keyBoard ) {
 		String input = "";
@@ -46,9 +57,102 @@ public class CubeExplorer {
 		
 	}
 	
+	
+	
 	private static boolean isValidCubeSize(String input) {
 		int cubeSize = Integer.parseInt(input);			
 		return cubeSize >= 1 || cubeSize <= 17;
 	}
+	
+	
+	
+	private static Cube scrambleCube(Cube cube) {
+		int cubeSize = cube.getSize();
+		int scrambleLength = 20;
+		Algorithm algo = new Algorithm();
+		algo.generateScramble(cubeSize, scrambleLength);
+		cube.applyAlgorithm(algo);
+		return cube;
+	}
+	
+	
+	/**
+	 * Takes user input and 
+	 * @param cube
+	 */
+	private static void startExplorer(Cube cube) {
+		Scanner keyBoard = new Scanner(System.in);
+		String input = keyBoard.nextLine();
+		boolean cubeChanged = false;
+		
+		while(!input.equals("quit")) {
+			if(input.equals("scramble")) {
+				cube = scrambleCube(cube);
+				cubeChanged = true;
+			}
+			else if(input.equals("solve")) {
+				System.out.println("Sorry, this feature is not yet available");
+				System.out.println("To generate a new cube type \"new\"\n");
+				
+			}
+			else if(input.equals("new")) {
+				int cubeSize = cube.getSize();
+				cube = new Cube(cubeSize);
+				cubeChanged = false;
+			}
+			else if(input.equals("size")) {
+				int cubeSize = getUsersCubeSize(keyBoard);
+				cube = new Cube(cubeSize);
+				cubeChanged = false;
+			}
+			else if(input.equals("usage")) {
+				displayCubeUsageMessage();
+			}
+			
+			else {
+				boolean isValidMove = isValidMove(input);
+				if(isValidMove) {
+					cube.rotate(new Move(input));
+					cubeChanged = true;
+				}
+				else {
+					System.out.println("Not a valid move. Type \"usage\" to view program usage");
+				}
+				
+			}
+			cubeChanged = didSolve(cube, cubeChanged);
+			System.out.println(cube);
+			input = keyBoard.nextLine();
+		}
+		
+		System.out.println("User Quit");
+	}
+	
+	
+	
+	public static boolean didSolve(Cube cube, boolean cubeChanged) {
+		System.out.println(cubeChanged);
+		if(cubeChanged) {
+			if(cube.isSolved()) {
+				System.out.println("CONGRATS, YOU SOLVED THE CUBE");
+				cubeChanged =false;
+			}
+		}
+		return cubeChanged;
+	}
+	
+	
+	
+	public static boolean isValidMove(String move) {
+		String FACE_TURN_NOTATION = "^([UDRLFB])([2']?)$";
+		String MIDDLE_TURN_NOTATION = "^([MES])([2']?)$";
+		String CUBE_ROTATION_NOTATION = "^([xyz])([2']?)$";
+		String INNER_SLICE_NOTATION = "^([udrlfb])([2']?)$";
+		String WIDE_TURN_NOTATION = "^([\\d]*)([UDRLFB])[w]([2']?)$";
+		return move.matches(FACE_TURN_NOTATION) | move.matches(MIDDLE_TURN_NOTATION)
+				| move.matches(CUBE_ROTATION_NOTATION) | move.matches(INNER_SLICE_NOTATION)
+				| move.matches(WIDE_TURN_NOTATION);
+	}
+	
 
 }
